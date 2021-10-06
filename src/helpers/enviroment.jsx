6576bcadd1ee85fbf9ba1190b816da1ae1c14df9
ChristianSpielman <cspielman10752@gmail.com>
2021-10-05 22:04:14 -0400

    Final

--- src/helpers/enviroment.jsx
+++ src/helpers/enviroment.jsx
@@ -4,14 +4,13 @@ switch(window.location.hostname){
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:4000';
         break;
-    case 'cms-red-badge-client.herokuapp.com'://needs to change
-        APIURL = 'https://cms-red-badge-server.herokuapp.com'//needs to change
+    case 'cms-red-badge-client.herokuapp.com':
+        APIURL = 'https://cms-red-badge-server.herokuapp.com';
         break;
 
         default:
             alert('url error');
             break;
 }
-// did not push case two changes yet
 
 export default APIURL
